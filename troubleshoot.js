e2f5d578ac4459914418854db9a2aee34193a66c
slp1604 <70539429+slp1604@users.noreply.github.com>
2020-10-22 23:48:07 -0500

    Update troubleshoot.js

--- troubleshoot.js
+++ troubleshoot.js
@@ -20,55 +20,50 @@ var fixPrinter = function() {
      * to the user.  In this case, Y-Y-Y means
      * "the printer does NOT print"  AND "a red light is flashing" AND "the printer is unrecognized"
      */
+    //Declare variables
     var boolPrinting = $("not_printing").selected;  //returns a value of true only when the not_printing html id is selected from the dropdown menu
     var boolRedLight = $("yes_redlight").selected;  //returns a value of true only when the yes_redlight html id is selected from the dropdown menu
     var boolRecognised = $("not_recognised").selected;  ////returns a value of true only when the not_recognized html id is selected from the dropdown menu
 
     //enter your code here and don't forget to send the output to the DOM
-if(boolPrinting === true)
-{
-    if(boolRedLight === true)
-    {
-        if(boolRecognised === true)
-        {
+if(boolPrinting === true) {
+    if(boolRedLight === true) {
+        if(boolRecognised === true) {
+            //Output for Y-Y-Y
             stringOutput = (stringMessage2 + "\n" + stringMessage3 + "\n" + stringMessage4);
-        } else
-            {
+        } else {
+                //Output for Y-Y-N
                 stringOutput = (stringMessage4 + "\n" + stringMessage5);
             }
-    } else
-        {
-            if(boolRecognised === true)
-                {
+    } else {
+            if(boolRecognised === true) {
+                    //Output for Y-N-Y
                     stringOutput = (stringMessage1 + "\n" + stringMessage2 + "\n" + stringMessage3);
-                } else
-                    {
+                } else {
+                        //Output for Y-N-N
                         stringOutput = (stringMessage5);
                     }
         }
-} else
-    {
-        if (boolRedLight === true)
-        {
-                if (boolRecognised === true)
-                    {
+} else {
+        if (boolRedLight === true) {
+                if (boolRecognised === true) {
+                        //Output for N-Y-Y
                         stringOutput = (stringMessage3 + "\n" + stringMessage4);
-                    } else
-                        {
+                    } else {
+                            //Output for N-Y-N
                             stringOutput = (stringMessage4);
                         }
-        } else
-            {
-                if (boolRecognised === true)
-                {
+        } else {
+                if (boolRecognised === true) {
+                    //Output for N-N-Y
                     stringOutput = (stringMessage3);
-                } else
-                    {
+                } else {
+                        //Output for N-N-N
                         stringOutput = (stringMessage6);
                     }
             }
     }   
-
+//Push results back to html
 $("output").value = stringOutput;
 
 };
